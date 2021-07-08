955bab0d703dbad34a28858849d4d6fc3296e41d
github username <github email address>
2021-07-07 20:10:49 -0400

    final commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import React from 'react'
 
 class App extends React.Component {
 
-  render() {
-    return (
-        <div className="App">
-          Garden Mate
-        </div>
-    );
-  }
+    renderPetals = () => {
+        let flower = document.createElement("div")
+
+        //
+        // let i = 0;
+        // while (i < 10) {
+        //     let petal = document.createElement("div")
+        //     petal.className = "petal"
+        //     petal.id = String(i)
+        //     flower.appendChild(petal)
+        //     i++
+        // }
+        let petal = document.createElement("div")
+        flower.appendChild(petal)
+        return flower[0]
+
+
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {this.renderPetals()}
+            </div>
+        );
+    }
 }
 
 export default App;
